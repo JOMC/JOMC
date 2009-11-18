e6711673661fc6e4732213263f0ff03258d8a9c6
Christian Schulte <cs@schulte.it>
2009-11-18 06:08:28 +0000

    [maven-scm] copy for tag jomc-parent-1.0-alpha-8
    git-svn-id: http://svn.jomc.org/svnroot/jomc/jomc/tags/jomc-parent-1.0-alpha-8@968 aee2edba-5628-49ba-b8b8-0e9d4ac63d57

--- jomc-api/src/main/java/org/jomc/ObjectManagementException.java
+++ jomc-api/src/main/java/org/jomc/ObjectManagementException.java
@@ -49,7 +49,7 @@ package org.jomc;
 // SECTION-START[Annotations]
 // <editor-fold defaultstate="collapsed" desc=" Generated Annotations ">
 @javax.annotation.Generated( value = "org.jomc.tools.JavaSources",
-                             comments = "See http://jomc.sourceforge.net/jomc/1.0-alpha-8-SNAPSHOT/jomc-tools" )
+                             comments = "See http://jomc.sourceforge.net/jomc/1.0-alpha-8/jomc-tools" )
 // </editor-fold>
 // SECTION-END
 public class ObjectManagementException extends RuntimeException
@@ -97,7 +97,7 @@ public class ObjectManagementException extends RuntimeException
 
     /** Creates a new {@code ObjectManagementException} instance. */
     @javax.annotation.Generated( value = "org.jomc.tools.JavaSources",
-                                 comments = "See http://jomc.sourceforge.net/jomc/1.0-alpha-8-SNAPSHOT/jomc-tools" )
+                                 comments = "See http://jomc.sourceforge.net/jomc/1.0-alpha-8/jomc-tools" )
     public ObjectManagementException()
     {
         // SECTION-START[Default Constructor]
