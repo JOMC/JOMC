521c0fa4ed96c1b52eb2f4f8c0297739c4e6d011
Christian Schulte <cs@schulte.it>
2010-04-27 12:30:16 +0000

    [maven-release-plugin] prepare for next development iteration
    git-svn-id: http://svn.jomc.org/svnroot/jomc/jomc/trunk@1817 aee2edba-5628-49ba-b8b8-0e9d4ac63d57

--- jomc-api/src/main/java/org/jomc/ObjectManagerFactory.java
+++ jomc-api/src/main/java/org/jomc/ObjectManagerFactory.java
@@ -52,7 +52,7 @@ import java.lang.reflect.Method;
 // SECTION-START[Annotations]
 // <editor-fold defaultstate="collapsed" desc=" Generated Annotations ">
 @javax.annotation.Generated( value = "org.jomc.tools.SourceFileProcessor",
-                             comments = "See http://jomc.sourceforge.net/jomc/1.0-beta-3-SNAPSHOT/jomc-tools" )
+                             comments = "See http://jomc.sourceforge.net/jomc/1.0-beta-4-SNAPSHOT/jomc-tools" )
 // </editor-fold>
 // SECTION-END
 public class ObjectManagerFactory
@@ -149,7 +149,7 @@ public class ObjectManagerFactory
 
     /** Creates a new {@code ObjectManagerFactory} instance. */
     @javax.annotation.Generated( value = "org.jomc.tools.SourceFileProcessor",
-                                 comments = "See http://jomc.sourceforge.net/jomc/1.0-beta-3-SNAPSHOT/jomc-tools" )
+                                 comments = "See http://jomc.sourceforge.net/jomc/1.0-beta-4-SNAPSHOT/jomc-tools" )
     public ObjectManagerFactory()
     {
         // SECTION-START[Default Constructor]
--- jomc-api/src/test/java/org/jomc/test/ObjectManagerFactoryTest.java
+++ jomc-api/src/test/java/org/jomc/test/ObjectManagerFactoryTest.java
@@ -52,7 +52,7 @@ import org.jomc.ObjectManagerFactory;
 // SECTION-START[Annotations]
 // <editor-fold defaultstate="collapsed" desc=" Generated Annotations ">
 @javax.annotation.Generated( value = "org.jomc.tools.SourceFileProcessor",
-                             comments = "See http://jomc.sourceforge.net/jomc/1.0-beta-3-SNAPSHOT/jomc-tools" )
+                             comments = "See http://jomc.sourceforge.net/jomc/1.0-beta-4-SNAPSHOT/jomc-tools" )
 // </editor-fold>
 // SECTION-END
 public class ObjectManagerFactoryTest
@@ -106,7 +106,7 @@ public class ObjectManagerFactoryTest
 
     /** Creates a new {@code ObjectManagerFactoryTest} instance. */
     @javax.annotation.Generated( value = "org.jomc.tools.SourceFileProcessor",
-                                 comments = "See http://jomc.sourceforge.net/jomc/1.0-beta-3-SNAPSHOT/jomc-tools" )
+                                 comments = "See http://jomc.sourceforge.net/jomc/1.0-beta-4-SNAPSHOT/jomc-tools" )
     public ObjectManagerFactoryTest()
     {
         // SECTION-START[Default Constructor]
