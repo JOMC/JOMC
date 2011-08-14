3778c46ebcb24537992f7ae586f019cd6d8bd620
Christian Schulte <cs@schulte.it>
2011-08-14 22:47:27 +0000

    [maven-scm-plugin] Build 2011-08-14T22:45:46+0000
    git-svn-id: http://svn.jomc.org/svnroot/jomc/jomc/trunk@3607 aee2edba-5628-49ba-b8b8-0e9d4ac63d57

--- jomc-spi/src/main/java/org/jomc/spi/Locator.java
+++ jomc-spi/src/main/java/org/jomc/spi/Locator.java
@@ -43,32 +43,26 @@ import java.net.URI;
 // <editor-fold defaultstate="collapsed" desc=" Generated Documentation ">
 /**
  * Locates objects.
+ *
  * <p>
- *   <table border="1" width="100%" cellpadding="3" cellspacing="0">
- *     <tr class="TableHeadingColor">
- *       <th align="left" scope="col" colspan="2" nowrap><font size="+2">Specification</font></th>
- *     </tr>
- *     <tr>
- *       <td class="TableSubHeadingColor" align="left" nowrap><b>Identifier:</b></td>
- *       <td class="TableRowColor" align="left" nowrap>{@code org.jomc.spi.Locator}</td>
- *     </tr>
- *     <tr>
- *       <td class="TableSubHeadingColor" align="left" nowrap><b>Multiplicity:</b></td>
- *       <td class="TableRowColor" align="left" nowrap>{@code Many}</td>
- *     </tr>
- *     <tr>
- *       <td class="TableSubHeadingColor" align="left" nowrap><b>Scope:</b></td>
- *       <td class="TableRowColor" align="left" nowrap>{@code Multiton}</td>
- *     </tr>
- *     <tr>
- *       <td class="TableSubHeadingColor" align="left" nowrap><b>Version:</b></td>
- *       <td class="TableRowColor" align="left" nowrap>{@code 1.0}</td>
- *     </tr>
- *   </table>
+ *   This specification is identified by identifier {@code org.jomc.spi.Locator}.
+ *   An application assembler may provide multiple implementations of this specification (including none) due to
+ *   multiplicity {@code Many}.
+ *   This specification does not apply to any scope. A new object is returned whenever requested.
+ * </p>
+ *
+ * <p>
+ *   Use of class {@code ObjectManager} is supported for accessing implementations.
+ *   <blockquote><pre>
+ * Locator[] objects = ObjectManagerFactory.getObjectManager( getClass().getClassLoader() ).getObject( Locator[].class );
+ * Locator object = ObjectManagerFactory.getObjectManager( getClass().getClassLoader() ).getObject( Locator.class, "<i>implementation name</i>" );
+ *   </pre></blockquote>
  * </p>
  *
  * @author <a href="mailto:schulte2005@users.sourceforge.net">Christian Schulte</a> 1.0
- * @version $Id$
+ * @version 1.0
+ * @see org.jomc.ObjectManagerFactory
+ * @see org.jomc.ObjectManager
  */
 // </editor-fold>
 // SECTION-END
