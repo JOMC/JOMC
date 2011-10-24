0eb633ca58218c654c41abffc20d6d017502d438
Christian Schulte <cs@schulte.it>
2011-10-24 01:20:33 +0000

    [maven-scm-plugin] Build 2011-10-24T01:17:03+0000
    git-svn-id: http://svn.jomc.org/svnroot/jomc/jomc/trunk@3884 aee2edba-5628-49ba-b8b8-0e9d4ac63d57

--- jomc-api/src/main/java/org/jomc/ObjectManagerFactory.java
+++ jomc-api/src/main/java/org/jomc/ObjectManagerFactory.java
@@ -163,7 +163,6 @@ public abstract class ObjectManagerFactory
     // SECTION-END
     // SECTION-START[Constructors]
     // <editor-fold defaultstate="collapsed" desc=" Generated Constructors ">
-
     /** Creates a new {@code ObjectManagerFactory} instance. */
     @javax.annotation.Generated( value = "org.jomc.tools.SourceFileProcessor 1.2-SNAPSHOT", comments = "See http://jomc.sourceforge.net/jomc/1.2/jomc-tools-1.2-SNAPSHOT" )
     public ObjectManagerFactory()
