eb0b3b71d0f2e318577c67ad1f7bb2ec7629a444
Christian Schulte <cs@schulte.it>
2012-02-26 04:04:15 +0000

    [maven-scm-plugin] Build 2012-02-26T04:03:39+0000
    git-svn-id: http://svn.jomc.org/svnroot/jomc/jomc/trunk@4296 aee2edba-5628-49ba-b8b8-0e9d4ac63d57

--- jomc-api/src/test/java/org/jomc/test/ObjectManagementExceptionTest.java
+++ jomc-api/src/test/java/org/jomc/test/ObjectManagementExceptionTest.java
@@ -60,7 +60,7 @@ import org.junit.Test;
 // SECTION-END
 // SECTION-START[Annotations]
 // <editor-fold defaultstate="collapsed" desc=" Generated Annotations ">
-@javax.annotation.Generated( value = "org.jomc.tools.SourceFileProcessor 2.0-SNAPSHOT", comments = "See http://jomc.sourceforge.net/jomc/1.2/jomc-tools-2.0-SNAPSHOT" )
+@javax.annotation.Generated( value = "org.jomc.tools.SourceFileProcessor 2.0-SNAPSHOT", comments = "See http://jomc.sourceforge.net/jomc/2.0/jomc-tools-2.0-SNAPSHOT" )
 // </editor-fold>
 // SECTION-END
 public class ObjectManagementExceptionTest
@@ -108,7 +108,7 @@ public class ObjectManagementExceptionTest
     // SECTION-START[Constructors]
     // <editor-fold defaultstate="collapsed" desc=" Generated Constructors ">
     /** Creates a new {@code ObjectManagementExceptionTest} instance. */
-    @javax.annotation.Generated( value = "org.jomc.tools.SourceFileProcessor 2.0-SNAPSHOT", comments = "See http://jomc.sourceforge.net/jomc/1.2/jomc-tools-2.0-SNAPSHOT" )
+    @javax.annotation.Generated( value = "org.jomc.tools.SourceFileProcessor 2.0-SNAPSHOT", comments = "See http://jomc.sourceforge.net/jomc/2.0/jomc-tools-2.0-SNAPSHOT" )
     public ObjectManagementExceptionTest()
     {
         // SECTION-START[Default Constructor]
