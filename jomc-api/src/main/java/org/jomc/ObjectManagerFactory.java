0eabcf2300b31dbe2cc78f78303be2a875fbeec2
Christian Schulte <cs@schulte.it>
2013-01-02 14:09:05 +0000

    [maven-release-plugin] prepare for next development iteration
    git-svn-id: http://svn.jomc.org/svnroot/jomc/jomc/branches/jomc-1.x@4714 aee2edba-5628-49ba-b8b8-0e9d4ac63d57

--- jomc-api/src/main/java/org/jomc/ObjectManagerFactory.java
+++ jomc-api/src/main/java/org/jomc/ObjectManagerFactory.java
@@ -55,7 +55,7 @@ package org.jomc;
 // SECTION-END
 // SECTION-START[Annotations]
 // <editor-fold defaultstate="collapsed" desc=" Generated Annotations ">
-@javax.annotation.Generated( value = "org.jomc.tools.SourceFileProcessor 1.4-SNAPSHOT", comments = "See http://www.jomc.org/jomc/1.4/jomc-tools-1.4-SNAPSHOT" )
+@javax.annotation.Generated( value = "org.jomc.tools.SourceFileProcessor 1.5-SNAPSHOT", comments = "See http://www.jomc.org/jomc/1.4/jomc-tools-1.5-SNAPSHOT" )
 // </editor-fold>
 // SECTION-END
 public abstract class ObjectManagerFactory
@@ -144,7 +144,7 @@ public abstract class ObjectManagerFactory
     // SECTION-START[Constructors]
     // <editor-fold defaultstate="collapsed" desc=" Generated Constructors ">
     /** Creates a new {@code ObjectManagerFactory} instance. */
-    @javax.annotation.Generated( value = "org.jomc.tools.SourceFileProcessor 1.4-SNAPSHOT", comments = "See http://www.jomc.org/jomc/1.4/jomc-tools-1.4-SNAPSHOT" )
+    @javax.annotation.Generated( value = "org.jomc.tools.SourceFileProcessor 1.5-SNAPSHOT", comments = "See http://www.jomc.org/jomc/1.4/jomc-tools-1.5-SNAPSHOT" )
     public ObjectManagerFactory()
     {
         // SECTION-START[Default Constructor]
