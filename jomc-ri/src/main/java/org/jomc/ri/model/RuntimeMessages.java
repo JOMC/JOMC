e7d4dd09772da5c288426bdbe4dca94df1667e33
Christian Schulte <cs@schulte.it>
2014-03-26 00:59:29 +0000

    [maven-release-plugin]  copy for tag jomc-parent-1.7
    git-svn-id: http://svn.jomc.org/svnroot/jomc/jomc/tags/jomc-parent-1.7@4948 aee2edba-5628-49ba-b8b8-0e9d4ac63d57

--- jomc-ri/src/main/java/org/jomc/ri/model/RuntimeMessages.java
+++ jomc-ri/src/main/java/org/jomc/ri/model/RuntimeMessages.java
@@ -64,7 +64,7 @@ import static org.jomc.ri.model.RuntimeModelObjects.createMap;
 // SECTION-END
 // SECTION-START[Annotations]
 // <editor-fold defaultstate="collapsed" desc=" Generated Annotations ">
-@javax.annotation.Generated( value = "org.jomc.tools.SourceFileProcessor 1.7-SNAPSHOT", comments = "See http://www.jomc.org/jomc/1.7/jomc-tools-1.7-SNAPSHOT" )
+@javax.annotation.Generated( value = "org.jomc.tools.SourceFileProcessor 1.7", comments = "See http://www.jomc.org/jomc/1.7/jomc-tools-1.7" )
 // </editor-fold>
 // SECTION-END
 public class RuntimeMessages extends Messages implements RuntimeModelObject
@@ -292,7 +292,7 @@ public class RuntimeMessages extends Messages implements RuntimeModelObject
     // SECTION-START[Constructors]
     // <editor-fold defaultstate="collapsed" desc=" Generated Constructors ">
     /** Creates a new {@code RuntimeMessages} instance. */
-    @javax.annotation.Generated( value = "org.jomc.tools.SourceFileProcessor 1.7-SNAPSHOT", comments = "See http://www.jomc.org/jomc/1.7/jomc-tools-1.7-SNAPSHOT" )
+    @javax.annotation.Generated( value = "org.jomc.tools.SourceFileProcessor 1.7", comments = "See http://www.jomc.org/jomc/1.7/jomc-tools-1.7" )
     public RuntimeMessages()
     {
         // SECTION-START[Default Constructor]
